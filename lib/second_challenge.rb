d7f2289c590affe9cb2d5fe531aa50a9ded4073c
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-13 22:41:11 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,15 @@ def second_challenge
       :patriarch => {
       :name => "Lord Montague",
       :age => "53"
-      } 
+      },
+      :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+      },
+        :hero => {
+          :name => "Romeo",
+          :age => "15"
+      }
     :capulet => {
       #your key/value pairs here!
           :patriarch => {
