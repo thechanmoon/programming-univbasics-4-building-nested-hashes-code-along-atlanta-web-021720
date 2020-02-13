f5aeffd5806201079c28b75efb0cadf2e05bb1c0
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-13 22:47:37 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,32 +2,26 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :patriarch => {
 
   },
     :matriarch => {
-      :name => "Lady Montague",
-      :age => "5
-  },4"
+
+  },
     :hero => {
-      :name => "Romeo",
-      :age => "15"
+
   },
     :hero_friends =>[]
       }, 
     :capulet => {
       #your key/value pairs here!
       :patriarch => {
-      :name => "Lord Capulet",
-      :age => "50"
+
     },
     :matriarch => {
-      :name => "Lady Capulet",
-      :age => "51"
     },
     :heroine => {
-      :name => "Juliet",
-      :age => "15",
-      :status => "alive"
+
     },
     :heroine_friends =>[]
       }}
