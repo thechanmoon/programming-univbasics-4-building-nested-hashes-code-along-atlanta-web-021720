fe0e16f609331dd68fdebc0825fa22b06436072c
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-13 23:06:19 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -14,7 +14,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Steven", age: "30", attitude: "confused"}, 
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
